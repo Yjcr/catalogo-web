fa4e355727227d8bfe814724772b37bbea6bd545
yeferson <yeferson26082004@gmail.com>
2024-05-07 14:49:03 -0400

    proyecto god

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('juegos', views.mostrar, name = 'home'), 
+    path('', views.mostrar, name = 'home'), 
     path('registro', views.registro, name = 'register'),
     path('cerra_sesion', views.cerrar_sesion, name = 'logout'),
     path('inicio_sesion', views.inicio_sesion, name = 'login')
