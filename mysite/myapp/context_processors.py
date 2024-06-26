0c4d82e7abdeed2e013373000f2c581f6ae0bb8d
DjGuetta <andwinmolina@gmail.com>
2024-06-26 00:29:20 -0400

    Implementando postgre y agregando toda la base de datos normalizada

--- mysite/myapp/context_processors.py
+++ mysite/myapp/context_processors.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Videogames, Category
+from .models import Videojuegos, Categorias
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
@@ -7,19 +7,16 @@ from django.db import IntegrityError
 from django.db.models import Q
 from django.core.paginator import Paginator
 
+
 def mostrar(request):
     query = request.GET.get("buscar")
-    juegos = Videogames.objects.all()  
-    categorias = Category.objects.all()
+    juegos = Videojuegos.objects.all()
+    categorias = Categorias.objects.all()
     if query:
-         juegos = Videogames.objects.filter(
-            Q(name__icontains = query) or 
-            Q(description__icontains = query)
-         ).distinct()
+        juegos = Videogames.objects.filter(
+            Q(name__icontains=query) or Q(description__icontains=query)
+        ).distinct()
     paginador = Paginator(juegos, 9)
-    page = request.GET.get('page')
+    page = request.GET.get("page")
     juegos = paginador.get_page(page)
-    return {'game':juegos, 
-            'query':query,
-            'categoria':categorias}
-
+    return {"game": juegos, "query": query, "categoria": categorias}
